7f1c20310ded747505169518cc6d4e3c24a0c757
Mohammad Arabzadeh <a.mohamad7824@gmail.com>
2024-04-09 19:53:27 -0400

    draw the route

--- PythonAPI/comp579/main.py
+++ PythonAPI/comp579/main.py
@@ -1,11 +1,31 @@
+import glob
+import os
+import sys
+
 import time
+
 from environment import CarlaEnv
+
+try:
+    sys.path.append(glob.glob('../carla/dist/carla-*%d.%d-%s.egg' % (
+        sys.version_info.major,
+        sys.version_info.minor,
+        'win-amd64' if os.name == 'nt' else 'linux-x86_64'))[0])
+except IndexError:
+    pass
+
+sys.path.append("../carla")
+
+import carla
+from agents.tools.misc import draw_locations
 
 def main():
     env = CarlaEnv()
-    env.reset()
+    draw_locations(env.world, env.route_points[:-1])
+    draw_locations(env.world, [env.route_points[-1]], color=carla.Color(0, 128, 0))
 
     try:
+        env.reset()
         
         states, reward, _, _ = env.step(0)
         print(f"R = {reward:.2f}")
